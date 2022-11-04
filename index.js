59a756362a5e21d6c794623eb44b4a5f1315c531
maheshkumar-wd <maheshkumaryadav1430@gmail.com>
2022-11-04 12:02:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.get("/",(req,res)=>{
+     res.send("Welcome to the mock server")
+})
+
 let PORT = process.env.PORT || 8080
 app.listen(PORT)
